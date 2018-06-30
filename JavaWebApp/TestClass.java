2838a25d5f1cfab56fd301474f81397d3fff2912
TripatSho <TripatSho@FCP-18721L.india.rapidigm.com>
2018-07-01 00:44:45 +0530

    Pulled and Pushing Updates!
    This is MR

---
+++ JavaWebApp/TestClass.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author TripatSho
+ *
+ */
+public class TestClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
